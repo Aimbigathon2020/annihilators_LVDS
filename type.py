12071eab38fcfae1d1780fea14c5a76ec1c6db60
nishanth-kumar0 <73699977+nishanth-kumar0@users.noreply.github.com>
2020-10-31 16:25:31 +0530

    Update type.py

--- type.py
+++ type.py
@@ -5,25 +5,18 @@ import numpy as np
 whT = 320
 confThreshold = 0.5
 nmsThreshold = 0.2
-
-#### LOAD MODEL
-## Coco Names
 classesFile = "coco.names"
 classNames = []
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
-print(classNames)
 COLORS= np.random.uniform(0, 255, size=(len(classNames), 3))
 vehiclewithplate=[2,3,5,7]
 vehiclewithoutplate=[1,8,4,6]
-## Model Files
 modelConfiguration = "yolov3.cfg"
 modelWeights = "yolov3.weights"
 net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
-
-
 def findObjects(outputs, img):
     hT, wT, cT = img.shape
     bbox = []
@@ -40,15 +33,13 @@ def findObjects(outputs, img):
                 bbox.append([x, y, w, h])
                 classIds.append(classId)
                 confs.append(float(confidence))
-
     indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
-
     for i in indices:
         i = i[0]
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
         color = COLORS[classIds[i]]
-        cv.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
         cv.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',(x, y +10), cv.FONT_HERSHEY_SIMPLEX, 0.6,color, 2)
 
 def typevehicle(img):
@@ -59,6 +50,6 @@ def typevehicle(img):
     outputs = net.forward(outputNames)
     findObjects(outputs, img)
     cv.imshow('Image', img)
-    k=cv.waitKey(0)
+    k=cv.waitKey(500)
     if(k==27):
         cv.destroyAllWindows()
